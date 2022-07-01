8838a794a34e65ca8c9d5741ee8c1997ca0e4568
David <david.shariev.2002@gmail.com>
2022-07-01 03:19:46 +0300

    2. Back-end: DTO

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,15 @@
 import { AppController } from './app.controller';
 import { Module } from "@nestjs/common"
 import { AppService } from './app.service';
+import { TrackModule } from './track/track.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
+    imports: [
+        MongooseModule.forRoot
+            ('mongodb+srv://admin:admin@cluster0.8p0yivi.mongodb.net/?retryWrites=true&w=majority'),
+        TrackModule
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
